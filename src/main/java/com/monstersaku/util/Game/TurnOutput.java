e8e7d035ec45819f649cefb7c07fed2b987d80a4
“reyshazni” <reyshazni@gmail.com>
2022-04-09 02:53:08 +0700

    update main

--- src/main/java/com/monstersaku/util/Game/TurnOutput.java
+++ src/main/java/com/monstersaku/util/Game/TurnOutput.java
@@ -1,8 +1,10 @@
 package com.monstersaku.util.Game;
 
+import java.util.Scanner;
+
 public interface TurnOutput {
     public void didStartAttacking();
-    public void playerOneTurn();
-    public void playerTwoTurn();
+    public void playerOneTurn(Scanner myObj);
+    public void playerTwoTurn(Scanner myObj);
     public void checkIfEndGame();
 }
